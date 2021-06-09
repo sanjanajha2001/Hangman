a5a0afba4d0985871051423b0d87166557094a45
sanjanajha2001 <sanjana20@navgurukul.org>
2021-06-09 11:37:53 +0530

    first commit

---
+++ words.py
@@ -0,0 +1,41 @@
+import string
+import random
+
+# def load_words():
+#     """
+    # Ye function kaafi jayada words ko load karne mai help karega
+    # """
+    # word_list = ["navgurukul", "learning", "kindness"]
+    # return word_list
+
+def load_words():
+    """
+    Ye function kaafi jayada words ko load karne mai help karega
+    """
+
+    # WORDLIST_FILENAME = "words.txt"
+    # print("Loading word list from file...")
+    # inFile = open("words.txt", 'r')
+    # line = inFile.readline()
+    # word_list = string.split(line)
+    # print( "  ", len(word_list), "words loaded.\n")
+    # return word_list 
+    wordlist=open("words.txt","r")
+    line=wordlist.read()
+    word_list=line.split()
+    # print()
+    return word_list
+
+#       
+def choose_word():
+    """
+    word_list (list): list of words (strings)
+    ye function ek word randomly return karega
+    """
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+
+    return secret_word
+# print(choose_word())
+
